f790c6ddbb84aab8c2dee08279d3545f8aade144
chansung park <deep.diver.csp@gmail.com>
2018-04-16 09:32:20 +0900

    make input field focusable when mounted in Search component.

--- app/src/Search.js
+++ app/src/Search.js
@@ -3,12 +3,20 @@ import {Link} from 'react-router-dom';
 import './Search.css';
 
 class Search extends Component {
+
+  componentDidMount(){
+    this.titleInput.focus();
+  }
+
   render() {
     return (
       <div>
         <div className="search-area">
           <Link to="/" className="back-button">back button</Link>
-          <input className="search-text" type="text"/>
+          <input
+            className="search-text"
+            type="text"
+            ref={(input) => { this.titleInput = input; }} />
         </div>
       </div>
     )
